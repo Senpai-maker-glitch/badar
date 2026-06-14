915df747e2822fe6f362069013ccfe4b96c62008
Senpai-maker-glitch <pathakaavash78@gmail.com>
2026-06-14 11:41:55 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+const messages = [
+  "You are really short 💖✨",
+  "You the only monkey in ma tree ",
+  "Stop being cute for no reason ",
+  "ik u call otherppl kukur me no special",
+  "Certified goofball ",
+  "You're kinda a dumbass ngl",
+  "Still short tho ",
+  "I love ya>:D",
+  "are u mine or am i urs?",
+  "lusty lesbian girl"
+];
+
+let lastIndex = -1;
+
 function showLove() {
-  document.getElementById("hiddenMessage").innerHTML =
-    "You are really short 💖✨";
+  let randomIndex;
+
+  do {
+    randomIndex = Math.floor(Math.random() * messages.length);
+  } while (randomIndex === lastIndex);
+
+  lastIndex = randomIndex;
+  document.getElementById("hiddenMessage").innerHTML = messages[randomIndex];
 }
